ab584c306b5f6476527dad39c1437b439bcc55ce
Adskiy Ponchik <adskiyponchik@gmail.com>
2025-09-07 22:22:47 +0200

    2 more tasks solved

---
+++ src/Leetcode/Choose_K_Elems.java
@@ -0,0 +1,10 @@
+package Leetcode;
+
+public class Choose_K_Elems {
+    public static class Solution {
+        public static long[] findMaxSum(int[] nums1, int[] nums2, int k) {
+            // TODO: do it next time
+            return new long[]{};
+        }
+    }
+}
---
+++ src/Leetcode/MergeIntervals.java
@@ -0,0 +1,24 @@
+package Leetcode;
+
+import java.util.*;
+
+public class MergeIntervals {
+    public static class Solution {
+        public int[][] merge(int[][] intervals) {
+            if (intervals.length == 0) return new int[0][0];
+            Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+            List<int[]> merged = new ArrayList<>();
+            merged.add(intervals[0]);
+            for(int i = 1; i < intervals.length; i++){
+                int[] lastInterval = merged.get(merged.size()-1);
+                int[] currentInterval = intervals[i];
+                if(lastInterval[1] >= currentInterval[0]){
+                    lastInterval[1] = Math.max(lastInterval[1], currentInterval[1]);
+                }else{
+                    merged.add(currentInterval);
+                }
+            }
+            return merged.toArray(new int[merged.size()][]);
+        }
+    }
+}
