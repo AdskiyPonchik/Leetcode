80bc50f2f1cb51fb0e8296c23227993eeec57d09
Adskiy Ponchik <adskiyponchik@gmail.com>
2025-09-27 16:23:20 +0200

    Codewars: beginning

---
+++ src/Codewars/PowerSumDigTerm.java
@@ -0,0 +1,14 @@
+package Codewars;
+
+//Difficulty: 5 kyu
+// Probably next time...
+public class PowerSumDigTerm {
+    public static long powerSumDigTerm(int n){
+        int counter = 0;
+        long num = 0;
+        while(counter!=n){
+
+        }
+        return 1;
+    }
+}
