d9ba9eedd4b5ee5e1c3013413c1c3d3a134958e9
adskiyponchik <adskiyponchik@gmail.com>
2026-04-07 20:06:30 +0200

    Tree tasks solved

---
+++ src/NeedCode/Tree/IsSameTree.java
@@ -0,0 +1,16 @@
+package NeedCode.Tree;
+
+public class IsSameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) &&
+                isSameTree(p.right, q.right);
+    }
+}
