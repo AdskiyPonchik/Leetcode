b0b9d5e7797ba8ec7dfa70ab32e48abc32a72f24
adskiyponchik <adskiyponchik@gmail.com>
2026-06-11 21:46:41 +0200

    Two tree tasks solved

---
+++ src/NeedCode/Tree/kthSmallest.java
@@ -0,0 +1,28 @@
+package NeedCode.Tree;
+
+import java.util.Stack;
+
+public class kthSmallest {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        int count = 0;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            count++;
+
+            if (count == k) {
+                return current.val;
+            }
+
+            current = current.right;
+        }
+        return -1;
+    }
+}
