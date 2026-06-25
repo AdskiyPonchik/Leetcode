8ff94ac3aaf9f5afcb0d13ec4341dd1f17ef84dd
adskiyponchik <adskiyponchik@gmail.com>
2026-06-25 18:33:19 +0200

    CombinationSum2

---
+++ src/NeedCode/Backtracking/CombinationSum2.java
@@ -0,0 +1,33 @@
+package NeedCode.Backtracking;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSum2 {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(0, candidates, target, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int start, int[] candidates, int target, List<Integer> current, List<List<Integer>> result) {
+        if(target == 0){
+            result.add(new ArrayList<>(current));
+        }
+        if(target<0){
+            return;
+        }
+        for(int i = start; i < candidates.length; i++){
+            if (i > start && candidates[i] == candidates[i - 1]) continue;
+            current.add(candidates[i]);
+
+            backtrack(i+1, candidates, target-candidates[i], current, result);
+
+            current.remove(current.size()-1);
+        }
+    }
+}
